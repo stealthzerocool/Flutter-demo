69a960cf9bff902460444a8ba23bf898f01dc534
stealthzerocool <ananthuk11@gmail.com>
2020-05-11 20:20:18 +0530

    images

--- lib/views/viewall.dart
+++ lib/views/viewall.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class Viewall extends StatelessWidget {
   var data = [
-    {"name": "reul", "age": 22, "admn": 232},
-    {"name": "Rahul", "age": 23, "admn": 2332},
-    {"name": "tom", "age": 26, "admn": 23562},
-    {"name": "tommm", "age":32, "admn": 23212}
+    {"image":"images/download.jpg","name": "reul", "age": 22, "admn": 232},
+    {"image":"images/images.jpg","name": "Rahul", "age": 23, "admn": 2332},
+    {"image":"images/images (1).jpg","name": "tom", "age": 26, "admn": 23562},
+    {"image":"images/images (2).jpg","name": "tommm", "age":32, "admn": 23212}
   ];
 
   @override
@@ -20,18 +20,25 @@ class Viewall extends StatelessWidget {
                 return Card(
                   elevation: 3.0,
                   child: ListTile(
-                  leading: Icon(Icons.access_alarm,size: 40.0,),
-                  trailing: int.parse(data[index]["age"].toString()) > 25 ? Icon(Icons.print):Icon(Icons.clear),
+                    leading: 
+                    ClipOval(
+                      child: Image(
+                        height: 90.0,
+                        width: 90.0,
+                        image: AssetImage(data[index]["image"]),
+                      ),
+                    ),
+                    trailing: int.parse(data[index]["age"].toString()) > 25 ? Icon(Icons.print):Icon(Icons.clear),
 
-                  title: Text(
-                    "Name :" + data[index]["name"],
-                    style: TextStyle(color: Colors.redAccent),
-                  ),
-                  subtitle: Text("Age :" +
-                      data[index]["age"].toString() +
-                      "\n" +
-                      "Admn no:" +
-                      data[index]["admn"].toString()),
+                    title: Text(
+                      "Name :" + data[index]["name"],
+                      style: TextStyle(color: Colors.redAccent),
+                    ),
+                    subtitle: Text("Age :" +
+                        data[index]["age"].toString() +
+                        "\n" +
+                        "Admn no:" +
+                        data[index]["admn"].toString()),
                   ),
                 );
               }),
