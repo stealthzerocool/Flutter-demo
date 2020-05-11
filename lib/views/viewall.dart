ae98fc28c1611db6bfe9e03f48e22d296ee3568c
stealthzerocool <ananthuk11@gmail.com>
2020-05-11 20:31:54 +0530

    net img

--- lib/views/viewall.dart
+++ lib/views/viewall.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 class Viewall extends StatelessWidget {
   var data = [
-    {"image":"images/download.jpg","name": "reul", "age": 22, "admn": 232},
-    {"image":"images/images.jpg","name": "Rahul", "age": 23, "admn": 2332},
-    {"image":"images/images (1).jpg","name": "tom", "age": 26, "admn": 23562},
-    {"image":"images/images (2).jpg","name": "tommm", "age":32, "admn": 23212}
+    {"image":"https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","name": "reul", "age": 22, "admn": 232},
+    {"image":"https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","name": "Rahul", "age": 23, "admn": 2332},
+    {"image":"https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","name": "tom", "age": 26, "admn": 23562},
+    {"image":"https://images.unsplash.com/photo-1518806118471-f28b20a1d79d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80","name": "tommm", "age":32, "admn": 23212}
   ];
 
   @override
@@ -25,7 +25,7 @@ class Viewall extends StatelessWidget {
                       child: Image(
                         height: 90.0,
                         width: 90.0,
-                        image: AssetImage(data[index]["image"]),
+                        image: NetworkImage(data[index]["image"]),
                       ),
                     ),
                     trailing: int.parse(data[index]["age"].toString()) > 25 ? Icon(Icons.print):Icon(Icons.clear),
