88252a33ee4ad1b81b3ea075c91337de80f36f0a
stealthzerocool <ananthuk11@gmail.com>
2020-05-16 10:43:59 +0530

    gird view

--- lib/views/pg1.dart
+++ lib/views/pg1.dart
@@ -1,11 +1,56 @@
 import 'package:flutter/material.dart';
 
+
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("Home page"),),
-
+      margin: EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+                hintText: "Enter Name",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))
+            ),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                hintText: "Enter Roll No:",
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))
+            ),
+          ),
+          SizedBox(
+            height: 12.0,
+          ),
+          GestureDetector(
+            onTap: (){
+              final msg= SnackBar(
+                  content: Text("Submit Clicked"));
+              Scaffold.of(context).showSnackBar(msg);
+            },
+            child: Container(
+              width: double.infinity,
+              height: 40.0,
+              decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(15.0)
+              ),
+              child: Center(
+                child: Text(
+                  "Submit",
+                  style: TextStyle(fontSize: 20.0,color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/views/pg2.dart
+++ lib/views/pg2.dart
@@ -1,11 +1,22 @@
-
 import 'package:flutter/material.dart';
 
+
 class Page2 extends StatelessWidget {
+  var names=["Antony","Ananthu","Anzar","Ismail","Antony","Ananthu","Anzar","Ismail","Antony","Ananthu","Anzar","Ismail"];
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(child: Text("List Page")),
+      child: GridView.builder(
+          itemCount: names.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2
+          ),
+          itemBuilder: (context,index){
+            return Card(
+              child: Center(child: Text(names[index])),
+            );
+          }
+      ),
     );
   }
 }
